678119b0ccc140591f6298299b696078a35bdd11
tanusrirramesh <tanusri.r.ramesh@gmail.com>
2026-01-19 21:10:04 +0530

    Update code.py

--- code.py
+++ code.py
@@ -29,22 +29,34 @@ def create():
             t.left(angle)
 
 root=tk.Tk()
+root.title("L-System Generator")
 
-canvas=tk.Canvas(root,width=400,height=400)
+canvas=tk.Canvas(root,width=700,height=400)
 canvas.pack()
 
 screen=turtle.TurtleScreen(canvas)
 t= turtle.RawTurtle(screen)
 t.speed(0)
 
-
+tk.Label(root, text="Rule").pack()
 ruleentry = tk.Entry(root)
 ruleentry.insert(0,"F+F")
 ruleentry.pack()
 
+tk.Label(root, text="Iterations").pack()
 iterentry=tk.Entry(root)
 iterentry.insert(0,"4")
 iterentry.pack()
 
-tk.Button(root, text="CREATE",command=generate).pack()
+tk.Label(root, text="Angle").pack()
+angleentry = tk.Entry(root)
+angleentry.insert(0, "90")
+angleentry.pack()
+
+tk.Label(root, text="Step Size").pack()
+stepentry = tk.Entry(root)
+stepentry.insert(0, "10")
+stepentry.pack()
+
+tk.Button(root, text="CREATE",command=create).pack()
 root.mainloop()
